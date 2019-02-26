98cca90e1e94dcac9646be50b33480db5caedbca
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-02-26 20:47:41 +0100

    Starterfiles

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include_once('nav.inc.php');
+
+    ?>
+
+    <main>
+        <h1>Contact</h1>
+    </main>
+    <footer>
+        &copy; IMD
+    </footer>    
+</body>
+</html>
---
+++ nav.inc.php
@@ -0,0 +1,10 @@
+<?php
+    $page = basename($_SERVER['SCRIPT_NAME']);
+    echo $page;
+?><header>
+    <nav>
+        <a class="navLink" href="index.php">Home</a>
+        <a class="navLink" href="contact.php">Contact</a>
+
+    </nav>
+</header>
